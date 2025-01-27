76688c594f12db5ac490106e275463c4d155f164
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-27 17:27:45 +0100

    test: test

--- surrealize/src/lib.rs
+++ surrealize/src/lib.rs
@@ -1,3 +1,18 @@
 pub use surrealize_derive::Surrealize;
 
 extern crate surrealize_derive;
+
+#[cfg(test)]
+mod test {
+    use serde::{Deserialize, Serialize};
+    use uuid::Uuid;
+
+    use crate::Surrealize;
+
+    #[derive(Serialize, Deserialize, Surrealize)]
+    struct TestStruct {
+        id: Uuid,
+        a: i32,
+        b: i32,
+    }
+}
